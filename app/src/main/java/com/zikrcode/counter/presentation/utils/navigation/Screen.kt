98b68d9f5ff0e52d8e449e673a462a5ca4475564
Zokirjon Mamadjonov <zikr.and.mehr@gmail.com>
2024-03-08 16:38:59 +0100

    enhanced sealed class screen

--- app/src/main/java/com/zikrcode/counter/presentation/utils/navigation/Screen.kt
+++ app/src/main/java/com/zikrcode/counter/presentation/utils/navigation/Screen.kt
@@ -18,11 +18,11 @@ package com.zikrcode.counter.presentation.utils.navigation
 
 sealed class Screen(val route: String) {
 
-    object CounterHomeScreen: Screen("counter_home_screen")
+    data object CounterHomeScreen: Screen("counter_home_screen")
 
-    object CounterListScreen: Screen("counter_list_screen")
+    data object CounterListScreen: Screen("counter_list_screen")
 
-    object CounterSettingsScreen: Screen("counter_settings_screen")
+    data object CounterSettingsScreen: Screen("counter_settings_screen")
 
-    object AddEditCounterScreen : Screen("add_edit_counter_screen")
+    data object AddEditCounterScreen : Screen("add_edit_counter_screen")
 }
