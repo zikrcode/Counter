98c9450fc4c8191bd496a53dd1161ea64d653269
Zokirjon Mamadjonov <zikrcode@gmail.com>
2025-11-11 14:53:15 +0100

    update LICENSE

--- app/src/main/java/com/zikrcode/counter/ui/screen/settings/PreferencesKey.kt
+++ app/src/main/java/com/zikrcode/counter/ui/screen/settings/PreferencesKey.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Zokirjon Mamadjonov
+ * Copyright (C) 2023–2025 Zokirjon Mamadjonov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
